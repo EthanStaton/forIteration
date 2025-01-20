05c2dd9881ea598bf63c42948fe4f3fe80a449a4
estaton <estaton@VB-PF2P7G6L>
2025-01-20 09:21:45 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bob's Big Giveaway!");
+        Console.Write("Choose 1, 2 or 3: ");
+
+
+        string userValue = Console.ReadLine();
+
+        string message = (userValue == "1") ? "boat" : "strand of lint";
+
+        Console.WriteLine($"You have input {userValue}, therefore you win a {message}");
+    }
+}
